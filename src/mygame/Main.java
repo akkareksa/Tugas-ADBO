30cb8b69a2c413d6625948c608bcdead9ee36034
Firman Sandy Prayitno <31910331+firmansandyp@users.noreply.github.com>
2017-11-18 17:33:34 +0700

    Update Main.java

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -28,6 +28,187 @@ private EventManager eventer;
 
     @Override
     public void simpleInitApp() {
+        NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        guiViewPort.addProcessor(niftyDisplay);
+        flyCam.setDragToRotate(true);
+
+        nifty.loadStyleFile("nifty-default-styles.xml");
+        nifty.loadControlFile("nifty-default-controls.xml");
+
+        nifty.addScreen("start", new ScreenBuilder("start") {
+            {
+                controller(new DefaultScreenController());
+                layer(new LayerBuilder("background") {
+                    {
+                        childLayoutCenter();
+                        backgroundColor("#000f");
+                        // <!-- ... -->
+                    }
+                });
+
+                layer(new LayerBuilder("foreground") {
+                    {
+                        childLayoutVertical();
+                        backgroundImage("Interface/trex.jpg");
+                        backgroundColor("#0000");
+
+                        // panel added
+                        panel(new PanelBuilder("panel_top") {
+                            {
+                                childLayoutCenter();
+                                alignCenter();
+//                                backgroundColor("#f008");
+                                height("25%");
+                                width("75%");
+                            }
+                        });
+
+                        panel(new PanelBuilder("panel_mid") {
+                            {
+                                childLayoutCenter();
+                                alignCenter();
+//                                backgroundColor("#0f08");
+                                height("50%");
+                                width("75%");
+                            }
+                        });
+
+                        panel(new PanelBuilder("panel_bottom") {
+                            {
+                                childLayoutHorizontal();
+                                alignCenter();
+//                                backgroundColor("#00f8");
+                                height("25%");
+                                width("75%");
+
+                                panel(new PanelBuilder("panel_bottom_left") {
+                                    {
+                                        childLayoutCenter();
+                                        valignCenter();
+//                                        backgroundColor("#44f8");
+                                        height("50%");
+                                        width("50%");
+
+                                        // add control
+                                        control(new ButtonBuilder("StartButton", "Play") {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                height("50%");
+                                                width("50%");
+                                                visibleToMouse(true);
+                                                interactOnClick("gotoScreen(hud)");
+                                            }
+                                        });
+
+                                    }
+                                });
+
+                                panel(new PanelBuilder("panel_bottom_right") {
+                                    {
+                                        childLayoutCenter();
+                                        valignCenter();
+//                                        backgroundColor("#88f8");
+                                        height("50%");
+                                        width("50%");
+
+                                        // add control
+                                        control(new ButtonBuilder("QuitButton", "Quit") {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                height("50%");
+                                                width("50%");
+                                                visibleToMouse(true);
+                                                interactOnClick("quitGame");
+                                            }
+                                        });
+
+                                    }
+                                });
+                            }
+                        }); // panel added
+                    }
+                });
+
+            }
+        }.build(nifty));
+
+        nifty.addScreen("hud", new ScreenBuilder("hud") {
+            {
+                controller(new DefaultScreenController());
+
+                layer(new LayerBuilder("background") {
+                    {
+                        childLayoutCenter();
+//                        backgroundColor("#000f");
+                        // <!-- ... -->
+                    }
+                });
+
+                layer(new LayerBuilder("foreground") {
+                    {
+                        childLayoutHorizontal();
+//                        backgroundColor("#0000");
+
+                        // panel added
+                        panel(new PanelBuilder("panel_left") {
+                            {
+                                childLayoutVertical();
+//                                backgroundColor("#0f08");
+                                height("100%");
+                                width("80%");
+                                // <!-- spacer -->
+                            }
+                        });
+
+                        panel(new PanelBuilder("panel_right") {
+                            {
+                                childLayoutVertical();
+//                                backgroundColor("#00f8");
+                                height("100%");
+                                width("20%");
+
+                                panel(new PanelBuilder("panel_top_right1") {
+                                    {
+                                        childLayoutCenter();
+//                                        backgroundColor("#00f8");
+                                        height("15%");
+                                        width("100%");
+                                    }
+                                });
+
+                                panel(new PanelBuilder("panel_top_right2") {
+                                    {
+                                        childLayoutCenter();
+//                                        backgroundColor("#44f8");
+                                        height("15%");
+                                        width("100%");
+                                    }
+                                });
+
+                                panel(new PanelBuilder("panel_bot_right") {
+                                    {
+                                        childLayoutCenter();
+                                        valignCenter();
+//                                        backgroundColor("#88f8");
+                                        height("70%");
+                                        width("100%");
+                                    }
+                                });
+                            }
+                        }); // panel added
+                    }
+                });
+            }
+        }.build(nifty));
+
+        nifty.gotoScreen("start");
+        
+        
+        
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
 
